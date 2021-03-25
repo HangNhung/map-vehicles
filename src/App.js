a3a413826d4eb96dfc19da1bab38b264c0f8402d
hbnhung265 <nhung.hb@vinstargroup.com>
2021-03-25 17:23:10 +0700

    intital map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
+import React from "react";
+//search
+import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
 import './App.css';
+import ReactMapGL, { NavigationControl, Marker, Source, Layer } from "react-map-gl";
+// import 'mapbox-gl/dist/mapbox-gl.css';
+import { geojson } from "./data/channel-bus";
+import Geocoder from 'react-map-gl-geocoder';
 
 function App() {
+  const navControlStyle= {
+    right: 10,
+    top: 10
+  };
+
+  let [viewport, setViewport] = React.useState({
+    longitude:  106.660172,
+    latitude:   10.762622,
+    zoom: 12
+  });
+
+  const layerStyle = {
+    id: 'my-layer',
+    type: 'circle',
+    source: 'points',
+    paint: {
+        'circle-color': '#f00',
+        'circle-radius': 25,
+    },
+  };
+
+  const mapRef = React.useRef();
+  const handleViewportChange = React.useCallback(
+    (newViewport) => setViewport(newViewport),
+    []
+  );
+
+    // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
+    const handleGeocoderViewportChange = React.useCallback(
+      (newViewport) => {
+        const geocoderDefaultOverrides = { transitionDuration: 1000 };
+   
+        return handleViewportChange({
+          ...newViewport,
+          ...geocoderDefaultOverrides
+        });
+      },
+      [] // eslint-disable-line
+    );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactMapGL 
+    ref={mapRef}
+    width="100vw" height="100vh"
+    {...viewport}
+    mapStyle="mapbox://styles/mapbox/streets-v11"
+   mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+   onViewportChange={handleViewportChange}
+   >
+       <Geocoder
+          mapRef={mapRef}
+          onViewportChange={handleGeocoderViewportChange}
+          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+          position="top-left"
+          language="vi"
+          limit={10}
+        />
+           <NavigationControl style={navControlStyle} />
+           <Source id="my-data" type="geojson" data={geojson}>
+            <Layer {...layerStyle}/>
+          </Source>
+     {/* {data.taxi.properties.map(item => (
+       <Marker key={item.TAXI_ID} longitude={item.LONGTITUDE} latitude={item.LATITUDE}>
+         <button style={{border: "none", background: "transparent"}}>
+           <img src="./marker.svg" alt="marker"/>
+         </button>
+       </Marker>
+     ))} */}
+      </ReactMapGL>
   );
 }
 
---
+++ src/data/channel-bus.js
@@ -0,0 +1,26 @@
+export const geojson = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "LineString",
+                "coordinates": [
+                    [
+                        106.6910636,
+                        10.7715341,
+                    ],
+                    [   106.695753,
+                        10.7904879,
+                    ],
+                    [   106.6975553,
+                        10.7675313,
+                    ],
+                    [   106.6676433,
+                        10.7929163,
+                    ],
+                ]
+            }
+        }
+    ]
+  }
