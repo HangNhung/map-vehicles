2c5cc355ba377617958d1aafc6b1fab1258f3068
Nhung Hang <hangbichnhung@gmail.com>
2021-03-26 00:54:05 +0700

    render data dynamic

--- src/App.js
+++ src/App.js
@@ -1,47 +1,59 @@
 import React from "react";
 //search
-import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
-import './App.css';
-import ReactMapGL, { NavigationControl, Marker, Source, Layer } from "react-map-gl";
+import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import "./App.css";
+import ReactMapGL, {
+  NavigationControl,
+  Marker,
+  Source,
+  Layer,
+} from "react-map-gl";
 // import 'mapbox-gl/dist/mapbox-gl.css';
 import { geojson } from "./data/channel-bus";
-import Geocoder from 'react-map-gl-geocoder';
-import myImage from './assets/marker.svg';
-
-var img = new Image(30,30);
-img.src = myImage;
+import Geocoder from "react-map-gl-geocoder";
+// import myImage from "./assets/marker.svg";
 
 function App() {
-  const navControlStyle= {
+  const [data, setData] = React.useState(null);
+  React.useEffect(() => {
+    // setInterval(() => {
+    //   setData(geojson());
+    // }, 3000);
+    setData(geojson());
+  }, []);
+  const navControlStyle = {
     right: 10,
-    top: 10
+    top: 10,
   };
 
   let [viewport, setViewport] = React.useState({
-    longitude:  106.660172,
-    latitude:   10.762622,
-    zoom: 12
+    longitude: 106.660172,
+    latitude: 10.762622,
+    zoom: 10,
   });
 
+  const layerStyle = {
+    id: "my-layer",
+    type: "circle",
+    source: "points",
+    paint: {
+      "circle-color": "#f00",
+      "circle-radius": 5,
+    },
+  };
+
   // const layerStyle = {
-  //   id: 'my-layer',
-  //   type: 'circle',
-  //   source: 'points',
-  //   paint: {
-  //       'circle-color': '#f00',
-  //       'circle-radius': 25,
+  //   id: "marker",
+  //   type: "symbol",
+  //   // source: 'points',
+  //   layout: {
+  //     "icon-image": "car-15",
+  //     "icon-anchor": "bottom",
+  //     "icon-offset": [0, 5],
+  //     "icon-allow-overlap": true,
+  //     "icon-size": 1.5,
   //   },
   // };
-
-  const layerStyle = {
-    id: 'marker',
-    type: 'symbol',
-    // source: 'points',
-    layout: { "icon-image": "car-15",   
-    'icon-anchor': 'bottom',
-    'icon-offset': [0, 5],
-    'icon-allow-overlap': true }
-  }
 
   const mapRef = React.useRef();
   const handleViewportChange = React.useCallback(
@@ -49,50 +61,50 @@ function App() {
     []
   );
 
-    // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
-    const handleGeocoderViewportChange = React.useCallback(
-      (newViewport) => {
-        const geocoderDefaultOverrides = { transitionDuration: 1000 };
-   
-        return handleViewportChange({
-          ...newViewport,
-          ...geocoderDefaultOverrides
-        });
-      },
-      [] // eslint-disable-line
-    );
+  // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
+  const handleGeocoderViewportChange = React.useCallback(
+    (newViewport) => {
+      const geocoderDefaultOverrides = { transitionDuration: 1000 };
+
+      return handleViewportChange({
+        ...newViewport,
+        ...geocoderDefaultOverrides,
+      });
+    },
+    [] // eslint-disable-line
+  );
 
   return (
-    <ReactMapGL   
-    ref={mapRef}
-    width="100vw" height="100vh"
-    {...viewport}
-    mapStyle="mapbox://styles/mapbox/streets-v11"
-   mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-   onViewportChange={handleViewportChange}
-   >
-       <Geocoder
-          mapRef={mapRef}
-          onViewportChange={handleGeocoderViewportChange}
-          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-          position="top-left"
-          language="vi"
-          limit={10}
-        />
-           <NavigationControl style={navControlStyle} />
-           <Source id="my-data" type="geojson" data={geojson}>
-            <Layer {...layerStyle}/>
-          </Source>
-     {/* {data.taxi.properties.map(item => (
+    <ReactMapGL
+      ref={mapRef}
+      width="100vw"
+      height="100vh"
+      {...viewport}
+      mapStyle="mapbox://styles/mapbox/streets-v11"
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+      onViewportChange={handleViewportChange}
+    >
+      <Geocoder
+        mapRef={mapRef}
+        onViewportChange={handleGeocoderViewportChange}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
+        position="top-left"
+        language="vi"
+        limit={10}
+      />
+      <NavigationControl style={navControlStyle} />
+      <Source id="my-data" type="geojson" data={data}>
+        <Layer {...layerStyle} />
+      </Source>
+      {/* {data.taxi.properties.map(item => (
        <Marker key={item.TAXI_ID} longitude={item.LONGTITUDE} latitude={item.LATITUDE}>
          <button style={{border: "none", background: "transparent"}}>
            <img src="./marker.svg" alt="marker"/>
          </button>
        </Marker>
      ))} */}
-      </ReactMapGL>
+    </ReactMapGL>
   );
-
-};
+}
 
 export default App;
--- src/data/channel-bus.js
+++ src/data/channel-bus.js
@@ -1,26 +1,50 @@
-export const geojson = {
-    "type": "FeatureCollection",
-    "features": [
-        {
-            "type": "Feature",
-            "geometry": {
-                "type": "LineString",
-                "coordinates": [
-                    [
-                        106.6910636,
-                        10.7715341,
-                    ],
-                    [   106.695753,
-                        10.7904879,
-                    ],
-                    [   106.6975553,
-                        10.7675313,
-                    ],
-                    [   106.6676433,
-                        10.7929163,
-                    ],
-                ]
-            }
-        }
-    ]
+// const countryBoundingBox = {
+//   latMin: 8.59975962975,
+//   latMax: 23.3520633001,
+//   longMin: 102.170435826,
+//   longMax: 109.33526981,
+// };
+
+const hcmBoundingBox = {
+  latMin: 10.73265,
+  latMax: 10.8422,
+  longMin: 106.620651,
+  longMax: 106.725327,
+};
+
+const renderData = (sizeData) => {
+  let data = [];
+  for (let i = 0; i < 20; i++) {
+    const latitude =
+      Math.random() * (hcmBoundingBox.latMax - hcmBoundingBox.latMin) +
+      hcmBoundingBox.latMin;
+    const longitude =
+      Math.random() * (hcmBoundingBox.longMax - hcmBoundingBox.longMin) +
+      hcmBoundingBox.longMin;
+    data.push([longitude, latitude]);
   }
+
+  console.log("data", data);
+  return data;
+};
+
+export const geojson = () => {
+  return {
+    type: "FeatureCollection",
+    features: [
+      {
+        type: "Feature",
+        geometry: {
+          type: "LineString",
+          // coordinates: [
+          //   [106.6910636, 10.7715341],
+          //   [106.695753, 10.7904879],
+          //   [106.6975553, 10.7675313],
+          //   [106.6676433, 10.7929163],
+          // ],
+          coordinates: renderData(20),
+        },
+      },
+    ],
+  };
+};
